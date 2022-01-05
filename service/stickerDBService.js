b3eb931ea08f88feb6374617ac6772ca7254972e
leecheng0115 <best.dev@outlook.com>
2022-01-05 17:47:26 +0900

    fix: getTranDetails apis are updated so that they can get platform in case of BuyOrder

--- service/stickerDBService.js
+++ service/stickerDBService.js
@@ -255,11 +255,9 @@ module.exports = {
             const response = await fetch(
                 'https://esc.elastos.io/api?module=transaction&action=gettxinfo&txhash=' + eventData.transactionHash
             );
-
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
-
             let data = await response.json();
             data = data.result;
             let transactionFee = data.gasUsed * data.gasPrice / (10 ** 18)
@@ -737,7 +735,7 @@ module.exports = {
             //type 0: total royalties, 1: total sales
             if(type == 0)
                 addressCondition.push({"sellerAddr": new RegExp('^' + walletAddr)});
-            else
+            else 
                 addressCondition.push({"royaltyOwner": new RegExp('^' + walletAddr)});
             let collection = client.db(config.dbName).collection('pasar_order');
             await collection.find({}).forEach( function (x) {
