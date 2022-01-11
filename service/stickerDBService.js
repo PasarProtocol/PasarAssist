b47523858c811b89df1e2862a047f1c54fb6be55
leecheng0115 <best.dev@outlook.com>
2022-01-12 01:20:07 +0900

    fix: getTotalRoyaltyandTotalSaleByWalletAddr fixed for primary sale ignore

--- service/stickerDBService.js
+++ service/stickerDBService.js
@@ -882,13 +882,13 @@ module.exports = {
             let result = await collection.aggregate([
                 { $match: {$and : [{$or :[...addressCondition]}, { 'orderState': '2'}]} },
                 { $sort: {updateTime: 1}},
-                { $project: {"_id": 0, orderId: 1, price: 1, royaltyFee: 1, updateTime: 1, amount: 1} },
+                { $project: {"_id": 0, royaltyOwner: 1, sellerAddr: 1, tokenId: 1, orderId: 1, price: 1, royaltyFee: 1, updateTime: 1, amount: 1} },
                 { $lookup: {from: "pasar_order_platform_fee", localField: "orderId", foreignField: "orderId", as: "platformFee"} },                
             ]).toArray();
             result.forEach(x => {
                 x.time = new Date(x.updateTime * 1000);
                 let platformFee = x.platformFee.length > 0 ? x.platformFee[0].platformFee: 0;
-                x.value = type == 1 ? parseInt(x.royaltyFee) : parseInt(x.price) * parseFloat(x.amount) - parseInt(platformFee);
+                x.value = type == 1 ? (x.sellerAddr == x.royaltyOwner? 0: parseInt(x.royaltyFee)) : parseInt(x.price) * parseFloat(x.amount) - parseInt(platformFee);
                 rows.push(x);
             });
             if(rows.length > 0) {
