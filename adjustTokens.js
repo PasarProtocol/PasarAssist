05f8e6a8e2a1420400079a93d3b9a868175a0230
cryptodev0115 <96402168+cryptodev0115@users.noreply.github.com>
2022-02-14 22:23:54 -0800

    merge commits from dev branch - mainly for updating token(#29)
    * fix:config_test.js
    
    * fix:some configs are updated to data accurate
    
    * fix:ipfs urls are fixed to use https instead of http
    
    * fix:search function is updated for tokenIdHex  to be keyword
    
    * feat:adjustTokens.js is added to avoid duplicate syncing
    
    * fix:adjustTokens.js
    
    * fix:adjustTokens is updated to get correct markettime and endtime

---
+++ adjustTokens.js
@@ -0,0 +1,13 @@
+let stickerDBService = require('./service/stickerDBService');
+let log4js = require('log4js');
+log4js.configure({
+    appenders: {
+        file: { type: 'dateFile', filename: 'logs/pasar.log', pattern: ".yyyy-MM-dd.log", compress: true, },
+        console: { type: 'stdout'}
+    },
+    categories: { default: { appenders: ['file', 'console'], level: 'info' } },
+    pm2: true,
+    pm2InstanceVar: 'INSTANCE_ID'
+});
+global.logger = log4js.getLogger('default');
+stickerDBService.updateTokens();
