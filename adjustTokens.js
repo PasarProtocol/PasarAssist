ffb1320c323cddd5cb7205f644089b2416ecc9a7
cryptodev0115 <best.dev@outlook.com>
2022-02-16 18:30:03 +0900

    fix:adjustTokens.js is updated to be able to exit when it finishes adjusting all tokens

--- adjustTokens.js
+++ adjustTokens.js
@@ -1,5 +1,6 @@
 let stickerDBService = require('./service/stickerDBService');
 let log4js = require('log4js');
+const { exit } = require('process');
 log4js.configure({
     appenders: {
         file: { type: 'dateFile', filename: 'logs/pasar.log', pattern: ".yyyy-MM-dd.log", compress: true, },
@@ -10,4 +11,6 @@ log4js.configure({
     pm2InstanceVar: 'INSTANCE_ID'
 });
 global.logger = log4js.getLogger('default');
-stickerDBService.updateTokens();
+stickerDBService.updateTokens().then(()=> {
+    exit();
+});
