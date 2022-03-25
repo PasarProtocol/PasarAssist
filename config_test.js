90b584d9eecc904c1a168bca3a2484f65a51657f
cryptodev0115 <best.dev@outlook.com>
2022-03-25 11:24:24 +0900

    Update: Update the contracs

--- config_test.js
+++ config_test.js
@@ -19,14 +19,19 @@ module.exports = {
     pasarContract: '0x2652d10A5e525959F7120b56f2D7a9cD0f6ee087',
     stickerContract: '0xed1978c53731997f4DAfBA47C9b07957Ef6F3961',
     galleriaContract: '0x8b3c7Fc42d0501e0367d29426421D950f45F5041',
-    pasarV2Contract: '0xcc30e6c72b42A9D9d77E8C87Cbd3b76E567796D5',
+    pasarV2Contract: '0xCD0A7bab14f8E6Ac3622ef8e0AD212f0855A7F36',
     stickerV2Contract: '0xcB13f1a8f68f17A7BCF91A946746930685ad233e',
+    dexSwapContract: '0xec4505B0B8C96e987684F76b6e087c3f823D7C09',
+    pasarRegisterContract: '0x8c2003cA415265fE142C951E17811b3Cb11A30D0',
+
     pasarContractDeploy: 7377671,
     stickerContractDeploy: 7377671,
-    pasarV2ContractDeploy: 10968238,
+    pasarV2ContractDeploy: 11264528,
     stickerV2ContractDeploy: 10968230,
     galleriaContractDeploy: 10242686,
-  
+    pasarRegisterContractDeploy: 11264526,
+    dexSwapContractDeploy: 11264530,
+
     ipfsNodeUrl: 'https://ipfs-test.pasarprotocol.io/ipfs/',
 
     serviceName: 'default',
---
+++ contractABI/dexSwapABI.js
@@ -0,0 +1,205 @@
+module.exports = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_codeAddress",
+        "type": "address"
+      }
+    ],
+    "name": "CodeUpdated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_orderId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_didUri",
+        "type": "string"
+      },
+      {
+        "internalType": "address[]",
+        "name": "_swapPath",
+        "type": "address[]"
+      }
+    ],
+    "name": "buyOrderWithETH",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_orderId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_didUri",
+        "type": "string"
+      },
+      {
+        "internalType": "address[]",
+        "name": "_swapPath",
+        "type": "address[]"
+      }
+    ],
+    "name": "buyOrderWithToken",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getCodeAddress",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "_codeAddress",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "initialize",
+    "outputs": [],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_pasarV2",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_uniswapV2",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_weth",
+        "type": "address"
+      }
+    ],
+    "name": "initialize",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "initialized",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes4",
+        "name": "_interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_newAddress",
+        "type": "address"
+      }
+    ],
+    "name": "updateCodeAddress",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+]
