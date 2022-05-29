85ee81cee7d6b678c899c663734d59610c644956
cryptodev0115 <best.dev@outlook.com>
2022-05-29 08:57:26 -0700

    Fix: Fix the issue of transfer

--- service/jobService.js
+++ service/jobService.js
@@ -163,8 +163,8 @@ module.exports = {
         } else if(tokenInfo._to == burnAddress) {
             await stickerDBService.burnToken(tokenId, token);
             await stickerDBService.replaceEvent(tokenEventDetail)
-        } else if(tokenEventDetail._to != config.pasarV2Contract && tokenEventDetail._to != config.pasarContract && tokenEventDetail._to != null
-            && tokenEventDetail._from != config.pasarV2Contract && tokenEventDetail._from != config.pasarContract && tokenEventDetail._from != null){
+        } else if(tokenInfo._to != config.pasarV2Contract && tokenInfo._to != config.pasarContract && tokenInfo._to != null
+            && tokenInfo._from != config.pasarV2Contract && tokenInfo._from != config.pasarContract && tokenInfo._from != null){
             tokenDetail.holder = tokenInfo._to;
             await stickerDBService.updateNormalToken(tokenDetail);
             await stickerDBService.replaceEvent(tokenEventDetail)
