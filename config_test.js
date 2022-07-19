c874bf267d451a18a0025fb866d5bd112aeafc5b
cryptodev0115 <best.dev@outlook.com>
2022-07-18 21:58:46 -0700

    Update: Update the contract address on esc network

--- config_test.js
+++ config_test.js
@@ -22,9 +22,9 @@ module.exports = {
     pasarContract: '0x2652d10A5e525959F7120b56f2D7a9cD0f6ee087',
     stickerContract: '0xed1978c53731997f4DAfBA47C9b07957Ef6F3961',
     galleriaContract: '0x8b3c7Fc42d0501e0367d29426421D950f45F5041',
-    pasarV2Contract: '0x19088c509C390F996802B90bdc4bFe6dc3F5AAA7',
-    stickerV2Contract: '0x32496388d7c0CDdbF4e12BDc84D39B9E42ee4CB0',
-    pasarRegisterContract: '0x2b304ffC302b402785294629674A8C2b64cEF897',
+    pasarV2Contract: '0xA9bBf0F9FB45Db6396D8e583DB69C13bA8C086A8',
+    stickerV2Contract: '0x68da537d387b874A1E06b7a951d622E4037F3680',
+    pasarRegisterContract: '0x930f675Ee9Bc62e37ef672c88158253dc65A63A0',
     diaTokenContract: '0x85946E4b6AB7C5c5C60A7b31415A52C0647E3272',
 
     pasarEthContract: '0x61EAE56bc110249648fB9eAe7eA4cfa185e0A498',
@@ -33,10 +33,10 @@ module.exports = {
 
     pasarContractDeploy: 7377671,
     stickerContractDeploy: 7377671,
-    pasarV2ContractDeploy: 12311847,
-    stickerV2ContractDeploy: 12311834,
+    pasarV2ContractDeploy: 13278867,
+    stickerV2ContractDeploy: 13278856,
     galleriaContractDeploy: 10242686,
-    pasarRegisterContractDeploy: 12311838,
+    pasarRegisterContractDeploy: 13278856,
     
     pasarEthContractDeploy: 12565400,
     stickerEthContractDeploy: 12549901,
